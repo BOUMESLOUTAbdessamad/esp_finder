0b4c35a8c9c47da076a283d93e545cba44269de4
Abdessamad <abdessamadbm31@gmail.com>
2020-08-12 18:51:48 +0100

    no auth.
    Footer patch.

--- resources/views/layouts/headers/nav.blade.php
+++ resources/views/layouts/headers/nav.blade.php
@@ -1,55 +1,4 @@
-{{-- <nav class="navbar navbar-expand-md navbar-light shadow-sm" >
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'USearch') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto" >
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('Wishlist') }}</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->firstname . ' '. Str::substr(Auth::user()->lastname , 0, 1).'.' }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
-</nav> --}}
 <nav class="navbar navbar-light navbar-expand-md shadow navigation-clean-search" style="background-color: #eeeeee;filter: invert(0%);">
     <div class="container"><a class="navbar-brand" href="{{ url('/') }}">USearch</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse"
@@ -64,7 +13,10 @@
                     <label for="search-field"><i class="fa fa-search"></i></label>
                     <input style="border-bottom-width: 2px !important; border-bottom-style: solid !important; border-bottom-color: #DDD !important;" class="form-control search-field" type="search" id="search-field" name="search">
                 </div>
+
             </form>
+
         </div>
+
     </div>
 </nav>
