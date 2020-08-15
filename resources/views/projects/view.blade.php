e786ba6999104b43313b4b5a98a0835afe67394c
Abdessamad <abdessamadbm31@gmail.com>
2020-08-15 13:00:31 +0100

    project view improvement.

--- resources/views/projects/view.blade.php
+++ resources/views/projects/view.blade.php
@@ -1,38 +1,29 @@
 @extends('layouts.app')
 
-@section('title', 'View')
+@section('title', $project->title)
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card shadow-sm">
-                {{-- <div class="card-header">{{ __('Dashboard') }}</div> --}}
-                <div class=" card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                        </div>
-                    @endif
+            <div class="card shadow-sm border-green " style="border-top-width: 6px !important; border-top-color: #f5de0d !important;">
+                <div class="card-body">
                     <div class="d-flex align-items-start flex-column">
-                         <h2>{{ $project->title }}</h2>
+                    <h2>{{ $project->title }}</h2>
                         <p class="text-gray">
                             Author : {{ $project->author }} <br> {{ $project->created }} <br>
                         </p>
                         <p class="text-gray">Status : {{ Str::ucfirst($project->status) }}</p>
                          <p><?=$project->content?></p>
                          <p>
-                            <?php
-                                $keywords = [];
-                                $keywords = explode(',', $project->keywords);
-                            ?>
-                                <b>Keywords : </b>
-                                @foreach ($keywords as $word)
-                                <a href="/search?q=<?=urlencode($word)?>" style="font-style: unset">
-                                    <span style="font-size: 10px; padding: 5px; color: #FFF" class="badge badge-info">
-                                        {{ $word }}
-                                    </span>
-                                </a>
-                                @endforeach
+                            <b>Keywords : </b>
+                            @foreach (explode(',', $project->keywords) as $word)
+                            <a href="/search?q=<?=urlencode($word)?>" style="font-style: unset">
+                                <span style="font-size: 10px; padding: 5px; color: #FFF" class="badge badge-info">
+                                    {{ $word }}
+                                </span>
+                            </a>
+                            @endforeach
                          </p>
                          <p>Author email : <a href="mailto:{{ $project->author_email }}">{{ $project->author_email }}</a></p>
                     </div>
